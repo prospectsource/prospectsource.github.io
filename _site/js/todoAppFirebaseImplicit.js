93677d1b7677998481758e8f9df11f42fdf917cd
rustyhutchison <rustywill7778@gmail.com>
2015-12-21 17:52:57 -0600

    go

--- _site/js/todoAppFirebaseImplicit.js
+++ _site/js/todoAppFirebaseImplicit.js
@@ -5,7 +5,7 @@ var TodoList3 = React.createClass({
     var createItem = function(item, index) {
       return (
         <li key={ index }>
-          { item.name } /** change the property after item." " to display the property you want */
+          { item.name /** change the property after item." " to display the property you want */ } 
           <span onClick={ _this.props.removeItem.bind(null, item['.key']) }
                 style={{ color: 'red', marginLeft: '10px', cursor: 'pointer' }}>
             X
