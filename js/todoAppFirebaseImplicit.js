15155a185ca0e07ab2188d3fc9618ac748ba6af3
rustyhutchison <rustywill7778@gmail.com>
2015-12-25 12:49:17 -0600

    g

--- js/todoAppFirebaseImplicit.js
+++ js/todoAppFirebaseImplicit.js
@@ -22,14 +22,14 @@ var TodoApp3 = React.createClass({
 
   getInitialState: function() {
     return {
-      items: [],
+      players: [],
       text: ''
     };
   },
 
   componentWillMount: function() {
     var firebaseRef = new Firebase('https://sweltering-fire-7944.firebaseio.com/players/');
-    this.bindAsArray(firebaseRef.limitToLast(25), 'items');
+    this.bindAsArray(firebaseRef.limitToLast(25), 'players');
   },
 
   onChange: function(e) {
@@ -56,7 +56,7 @@ var TodoApp3 = React.createClass({
   render: function() {
     return (
       <div>
-        <TodoList3 items={ this.state.items } removeItem={ this.removeItem } />
+        <TodoList3 items={ this.state.players } removeItem={ this.removeItem } />
         <form onSubmit={ this.handleSubmit }>
           <input onChange={ this.onChange } value={ this.state.text } />
           <button>{ 'Add #' + (this.state.items.length + 1) }</button>
