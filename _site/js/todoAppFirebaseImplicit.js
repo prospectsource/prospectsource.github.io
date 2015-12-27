ca2fcf21cfff86703b47dc9dc81258a932b6829c
rustyhutchison <rustywill7778@gmail.com>
2015-12-27 14:00:21 -0600

    go

--- _site/js/todoAppFirebaseImplicit.js
+++ _site/js/todoAppFirebaseImplicit.js
@@ -64,7 +64,7 @@ var TodoApp3 = React.createClass({
       <div>
         <TodoList3 items={ this.state.items } removeItem={ this.removeItem } />
         <form onSubmit={ this.handleSubmit }>
-          <input onChange={ this.onChange } value={ this.state.text } />
+          <input onChange={ this.onChange } value={ this.state.text } /><br>
           <input onChange={ this.onChange2 } value={ this.state.text2 } />
           <button>{ 'Add #' + (this.state.items.length + 1) }</button>
         </form>
