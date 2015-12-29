05ed93be9c7d975bace3789075a213c97fc295ba
rustyhutchison <rustywill7778@gmail.com>
2015-12-29 13:23:27 -0600

    go

--- _site/js/playerFilter.js
+++ _site/js/playerFilter.js
@@ -1,4 +1,5 @@
 /** @jsx React.DOM */
+
 var ProductCategoryRow = React.createClass({
     render: function() {
         return (<tr><th colSpan="2">{this.props.category}</th></tr>);
@@ -26,15 +27,12 @@ var ProductTable = React.createClass({
         var rows = [];
         var lastCategory = null;
         this.props.products.forEach(function(product) {
-            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
-                return;
-            }
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
             }
             rows.push(<ProductRow product={product} key={product.name} />);
             lastCategory = product.category;
-        }.bind(this));
+        });
         return (
             <table>
                 <thead>
@@ -50,29 +48,12 @@ var ProductTable = React.createClass({
 });
 
 var SearchBar = React.createClass({
-    handleChange: function() {
-        this.props.onUserInput(
-            this.refs.filterTextInput.value,
-            this.refs.inStockOnlyInput.checked
-        );
-    },
     render: function() {
         return (
             <form>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={this.props.filterText}
-                    ref="filterTextInput"
-                    onChange={this.handleChange}
-                />
+                <input type="text" placeholder="Search..." />
                 <p>
-                    <input
-                        type="checkbox"
-                        checked={this.props.inStockOnly}
-                        ref="inStockOnlyInput"
-                        onChange={this.handleChange}
-                    />
+                    <input type="checkbox" />
                     {' '}
                     Only show products in stock
                 </p>
@@ -82,33 +63,11 @@ var SearchBar = React.createClass({
 });
 
 var FilterableProductTable = React.createClass({
-    getInitialState: function() {
-        return {
-            filterText: '',
-            inStockOnly: false
-        };
-    },
-
-    handleUserInput: function(filterText, inStockOnly) {
-        this.setState({
-            filterText: filterText,
-            inStockOnly: inStockOnly
-        });
-    },
-
     render: function() {
         return (
             <div>
-                <SearchBar
-                    filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly}
-                    onUserInput={this.handleUserInput}
-                />
-                <ProductTable
-                    products={this.props.products}
-                    filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly}
-                />
+                <SearchBar />
+                <ProductTable products={this.props.products} />
             </div>
         );
     }
@@ -123,7 +82,7 @@ var PRODUCTS = [
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
 ];
-
+ 
 React.render(
     <FilterableProductTable products={PRODUCTS} />,
     document.getElementById('container')
