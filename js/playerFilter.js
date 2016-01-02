df3509ea0b28c50198a6a607062667d2313091c5
rustyhutchison <rustywill7778@gmail.com>
2016-01-02 14:47:54 -0600

    go

--- js/playerFilter.js
+++ js/playerFilter.js
@@ -28,7 +28,7 @@ var ProductTable = React.createClass({
         var lastCategory = null;
         this.props.products.forEach(function(product) {
             
-            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+            if (product.name.toLowerCase.indexOf(this.props.filterText) === -1 || (!product.stocked.toLowerCase && this.props.inStockOnly.toLowerCase)) {
                 return;
             }
             
