6819e25dcec36628ae4d5e5ba144dcd377cc718f
rustyhutchison <rustywill7778@gmail.com>
2016-01-03 20:37:05 -0600

    go

--- js/playerFilter.js
+++ js/playerFilter.js
@@ -129,6 +129,8 @@ var PRODUCTS = [
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
 ];
+
+var PRODUCTS = PRODUCTS.toLowerCase();
  
 React.render(
     <FilterableProductTable products={PRODUCTS} />,
