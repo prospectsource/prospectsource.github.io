0ecf8ae8a7df55c0137819bbddda7e622c741060
rustyhutchison <rustywill7778@gmail.com>
2016-01-17 01:52:46 -0600

    go

---
+++ js/prospect-form-submission-handler.js
@@ -0,0 +1,45 @@
+// get all data in form and return object
+function getFormData() {
+  var data   = {
+    first    : document.getElementById("first").value,
+    last   : document.getElementById("last").value,
+    home : document.getElementById("home").value,
+    email : document.getElementById("email").value,
+    phone : document.getElementById("phone").value,
+    church : document.getElementById("church").value,
+    reason : document.getElementById("reason").value
+  }
+  console.log(data);
+  return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+  event.preventDefault();           // we are submitting via xhr below
+  var data = getFormData();         // get the values submitted in the form
+  
+    var url = event.target.action;  //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        console.log( xhr.status, xhr.statusText )
+        console.log(xhr.responseText);
+        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('thankyou_message').style.display = 'block';
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+  
+}
+function loaded() {
+  console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  var form = document.getElementById('gform');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener('DOMContentLoaded', loaded, false);
