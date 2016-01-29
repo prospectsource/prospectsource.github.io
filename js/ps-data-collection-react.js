8361acf29fe0e8460ea1f46a719b9e7eaf6a6c53
rustyhutchison <rustywill7778@gmail.com>
2016-01-29 11:35:47 -0600

    go

--- js/ps-data-collection-react.js
+++ js/ps-data-collection-react.js
@@ -1,4 +1,6 @@
 /** @jsx React.DOM */
+
+
 var TodoList3 = React.createClass({
   render: function() {
     var _this = this;
@@ -73,15 +75,65 @@ var TodoApp3 = React.createClass({
 
   render: function() {
     return (
-      <div>
-        <TodoList3 items={ this.state.items } removeItem={ this.removeItem } />
-        <form onSubmit={ this.handleSubmit }>
-          <div>Name: <input onChange={ this.onChange } value={ this.state.text } /></div>
-          <div>Position: <input onChange={ this.onChange2 } value={ this.state.text2 } /></div>
-          <div>Height: <input onChange={ this.onChange3 } value={ this.state.text3 } /></div>
-          <button>{ 'Add #' + (this.state.items.length + 1) }</button>
+      
+      <section id="data-collection">
+    	<div className="container">
+    		<div className="row">
+    			<div className="col-sm-12">
+    				<div className="timeline-image1">
+    				</div>
+    				<div className="timeline-image2">
+    				</div>
+    				<div className="timeline-image3">
+    				</div>
+    			</div>
+    		</div>
+    		<div className="row">
+    			<div className="col-sm-4 input-box">
+    				<div className="input-container">
+            			<h4>CONTACT INFORMATION</h4>
+            			<div>PHONE (ATHLETE) <input onChange={ this.onChange } value={ this.state.text } /></div>
+          				<div>EMAIL (ATHLETE OR PARENT) <input onChange={ this.onChange2 } value={ this.state.text2 } /></div>
+          				<div>ADDRESS <input onChange={ this.onChange3 } value={ this.state.text3 } /></div>
+          				<div>CITY/STATE/ZIP <input onChange={ this.onChange4 } value={ this.state.text4 } /></div>
+          				<div>AAU COACH NAME <input onChange={ this.onChange5 } value={ this.state.text5 } /></div>
+          				<div>AAU COACH EMAIL <input onChange={ this.onChange6 } value={ this.state.text6 } /></div>
+          				<div>AAU COACH PHONE <input onChange={ this.onChange7 } value={ this.state.tex7 } /></div>
+          				<div>HS COACH NAME <input onChange={ this.onChange8 } value={ this.state.text8 } /></div>
+          				<div>HS COACH EMAIL <input onChange={ this.onChange9 } value={ this.state.text9 } /></div>
+          				<div>HS COACH PHONE <input onChange={ this.onChange10 } value={ this.state.text10 } /></div>
+       				 </div>
+    			</div>
+    			<div className="col-sm-4 input-box">
+    				<div className="input-container">
+            			<h4>ATHLETIC INFORMATION</h4>
+            			<div>GRADE/CLASS <input onChange={ this.onChange11 } value={ this.state.text11 } /></div>
+          				<div>HEIGHT <input onChange={ this.onChange12 } value={ this.state.text12 } /></div>
+          				<div>WEIGHT <input onChange={ this.onChange13 } value={ this.state.text13 } /></div>
+          				<div>VERTICAL JUMP <input onChange={ this.onChange14 } value={ this.state.text14 } /></div>
+          				<div>POSITION <input onChange={ this.onChange15 } value={ this.state.text15 } /></div>
+          				<div>AAU PROGRAM <input onChange={ this.onChange16 } value={ this.state.text16 } /></div>
+          				<div>AAU JERSEY <input onChange={ this.onChange17 } value={ this.state.text17 } /></div>
+          				<div>HIGH SCHOOL <input onChange={ this.onChange18 } value={ this.state.text18 } /></div>
+          				<div>HUDL PROFILE <input onChange={ this.onChange19 } value={ this.state.text19 } /></div>
+       				 </div>
+    			</div>
+    			<div className="col-sm-4 input-box">
+    				<div className="input-container">
+            			<h4>ACADEMIC INFORMATION</h4>
+            			<div>GPA <input onChange={ this.onChange20 } value={ this.state.text20 } /></div>
+          				<div>ACT <input onChange={ this.onChange21 } value={ this.state.text21 } /></div>
+          				<div>CLASS RANK <input onChange={ this.onChange22 } value={ this.state.text22 } /></div>
+       				</div>
+    			</div>
+    		</div>
+    	</div>
+    	
+        <form className="text-center" onSubmit={ this.handleSubmit }>
+          
+          <button className="btn btn-default btn-large">{ 'Add #' + (this.state.items.length + 1) }</button>
         </form>
-      </div>
+      </section>
     );
   }
 });
