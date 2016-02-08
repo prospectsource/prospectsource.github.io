d23e3e5ff900e36e549228e6feff798e5ecdf473
rustyhutchison <rustywill7778@gmail.com>
2016-02-08 10:08:38 -0600

    go

--- js/ps-data-collection-react.js
+++ js/ps-data-collection-react.js
@@ -37,18 +37,101 @@ var TodoApp3 = React.createClass({
     this.bindAsArray(firebaseRef.limitToLast(25), 'items');
   },
 
-  onChange: function(e) {
-    this.setState({text: e.target.value});
+  onFirstChange: function(e) {
+    this.setState({first: e.target.value});
   },
   
-  onChange2: function(e) {
-    this.setState({text2: e.target.value});
+  onLastChange: function(e) {
+    this.setState({last: e.target.value});
   },
   
-  onChange3: function(e) {
-    this.setState({text3: e.target.value});
+  onPhoneChange: function(e) {
+    this.setState({phone: e.target.value});
   },
   
+  onEmailChange: function(e) {
+    this.setState({email: e.target.value});
+  },
+  
+  onAddressChange: function(e) {
+    this.setState({address: e.target.value});
+  },
+  
+  onCityStateZipChange: function(e) {
+    this.setState({cityStateZip: e.target.value});
+  },
+  
+  onAauCoachChange: function(e) {
+    this.setState({aauCoach: e.target.value});
+  },
+  
+  onAauEmailChange: function(e) {
+    this.setState({aauEmail: e.target.value});
+  },
+  
+  onAauPhoneChange: function(e) {
+    this.setState({aauPhone: e.target.value});
+  },
+  
+  onHsCoachChange: function(e) {
+    this.setState({hsCoach: e.target.value});
+  },
+  
+  onHsEmailChange: function(e) {
+    this.setState({hsEmail: e.target.value});
+  },
+  
+  onHsPhoneChange: function(e) {
+    this.setState({hsPhone: e.target.value});
+  },
+  
+  onGradeChange: function(e) {
+    this.setState({grade: e.target.value});
+  },
+  
+  onHeightChange: function(e) {
+    this.setState({height: e.target.value});
+  },
+  
+  onWeightChange: function(e) {
+    this.setState({weight: e.target.value});
+  },
+  
+  onVertChange: function(e) {
+    this.setState({vert: e.target.value});
+  },
+  
+  onPositionChange: function(e) {
+    this.setState({position: e.target.value});
+  },
+  
+  onAauProgramChange: function(e) {
+    this.setState({aauProgram: e.target.value});
+  },
+  
+  onAauJerseyChange: function(e) {
+    this.setState({aauJersey: e.target.value});
+  },
+  
+  onSchoolChange: function(e) {
+    this.setState({school: e.target.value});
+  },
+  
+  onHudlChange: function(e) {
+    this.setState({hudl: e.target.value});
+  },
+  
+  onGpaChange: function(e) {
+    this.setState({gpa: e.target.value});
+  },
+  
+  onActChange: function(e) {
+    this.setState({act: e.target.value});
+  },
+  
+  onClassRankChange: function(e) {
+    this.setState({classRank: e.target.value});
+  },
   
   removeItem: function(key) {
     var firebaseRef = new Firebase('https://sweltering-fire-7944.firebaseio.com/players/');
@@ -59,9 +142,30 @@ var TodoApp3 = React.createClass({
     e.preventDefault();
     if (this.state.text && this.state.text.trim().length !== 0) {
       this.firebaseRefs['items'].push({
-        name: this.state.text,
-        position: this.state.text2, // "name:" changes the input attribute category
-        height: this.state.text3
+        first: this.state.first,// "name:" changes the input attribute category
+        last: this.state.last, 
+        phone: this.state.phone,
+        email: this.state.email, 
+        address: this.state.address,
+        cityStateZip: this.state.cityStateZip, 
+        aauCoach: this.state.aauCoach,
+        aauEmail: this.state.aauEmail, 
+        aauPhone: this.state.aauPhone,
+        hsCoach: this.state.hsCoach, 
+        hsEmail: this.state.hsEmail,
+        hsPhone: this.state.hsPhone, 
+        grade: this.state.grade,
+        height: this.state.height, 
+        weight: this.state.weight,
+        vert: this.state.vert, 
+        position: this.state.position,
+        aauProgram: this.state.aauProgram, 
+        aauJersey: this.state.aauJersey,
+        school: this.state.school, 
+        hudl: this.state.hudl,
+        gpa: this.state.gpa,
+        act: this.state.act, 
+        classRank: this.state.classRank
       });
       this.setState({
         name: '',
@@ -103,40 +207,40 @@ var TodoApp3 = React.createClass({
     			<div className="col-md-4 input-box">
     				<div className="input-container">
             			<h4>CONTACT INFORMATION</h4>
-            			<div>FIRST NAME <input onChange={ this.onChange } value={ this.state.text } /></div>
-						<div>LAST NAME <input onChange={ this.onChange } value={ this.state.text } /></div>            			
-            			<div>PHONE (ATHLETE) <input onChange={ this.onChange } value={ this.state.text } /></div>
-          				<div>EMAIL (ATHLETE OR PARENT) <input onChange={ this.onChange2 } value={ this.state.text2 } /></div>
-          				<div>ADDRESS <input onChange={ this.onChange3 } value={ this.state.text3 } /></div>
-          				<div>CITY/STATE/ZIP <input onChange={ this.onChange4 } value={ this.state.text4 } /></div>
-          				<div>AAU COACH NAME <input onChange={ this.onChange5 } value={ this.state.text5 } /></div>
-          				<div>AAU COACH EMAIL <input onChange={ this.onChange6 } value={ this.state.text6 } /></div>
-          				<div>AAU COACH PHONE <input onChange={ this.onChange7 } value={ this.state.tex7 } /></div>
-          				<div>HS COACH NAME <input onChange={ this.onChange8 } value={ this.state.text8 } /></div>
-          				<div>HS COACH EMAIL <input onChange={ this.onChange9 } value={ this.state.text9 } /></div>
-          				<div>HS COACH PHONE <input onChange={ this.onChange10 } value={ this.state.text10 } /></div>
+            			<div>FIRST NAME <input onChange={ this.onFirstChange } value={ this.state.first } /></div>
+						<div>LAST NAME <input onChange={ this.onLastChange } value={ this.state.last } /></div>            			
+            			<div>PHONE (ATHLETE) <input onChange={ this.onPhoneChange } value={ this.state.phone } /></div>
+          				<div>EMAIL (ATHLETE OR PARENT) <input onChange={ this.onEmailChange } value={ this.state.email } /></div>
+          				<div>ADDRESS <input onChange={ this.onAddressChange } value={ this.state.address } /></div>
+          				<div>CITY/STATE/ZIP <input onChange={ this.onCityStateZipChange } value={ this.state.cityStateZip } /></div>
+          				<div>AAU COACH NAME <input onChange={ this.onAauCoachChange } value={ this.state.aauCoach } /></div>
+          				<div>AAU COACH EMAIL <input onChange={ this.onAauEmailChange } value={ this.state.aauEmail } /></div>
+          				<div>AAU COACH PHONE <input onChange={ this.onAauPhoneChange } value={ this.state.aauPhone } /></div>
+          				<div>HS COACH NAME <input onChange={ this.onHsCoachChange } value={ this.state.hsCoach } /></div>
+          				<div>HS COACH EMAIL <input onChange={ this.onHsEmailChange } value={ this.state.hsEmail } /></div>
+          				<div>HS COACH PHONE <input onChange={ this.onHsPhoneChange } value={ this.state.hsPhone } /></div>
        				 </div>
     			</div>
     			<div className="col-md-4 input-box">
     				<div className="input-container">
             			<h4>ATHLETIC INFORMATION</h4>
-            			<div>GRADE/CLASS <input onChange={ this.onChange11 } value={ this.state.text11 } /></div>
-          				<div>HEIGHT <input onChange={ this.onChange12 } value={ this.state.text12 } /></div>
-          				<div>WEIGHT <input onChange={ this.onChange13 } value={ this.state.text13 } /></div>
-          				<div>VERTICAL JUMP <input onChange={ this.onChange14 } value={ this.state.text14 } /></div>
-          				<div>POSITION <input onChange={ this.onChange15 } value={ this.state.text15 } /></div>
-          				<div>AAU PROGRAM <input onChange={ this.onChange16 } value={ this.state.text16 } /></div>
-          				<div>AAU JERSEY <input onChange={ this.onChange17 } value={ this.state.text17 } /></div>
-          				<div>HIGH SCHOOL <input onChange={ this.onChange18 } value={ this.state.text18 } /></div>
-          				<div>HUDL PROFILE <input onChange={ this.onChange19 } value={ this.state.text19 } /></div>
+            			<div>GRADE/CLASS <input onChange={ this.onGradeChange } value={ this.state.grade } /></div>
+          				<div>HEIGHT <input onChange={ this.onHeightChange } value={ this.state.height } /></div>
+          				<div>WEIGHT <input onChange={ this.onWeightChange } value={ this.state.weight } /></div>
+          				<div>VERTICAL JUMP <input onChange={ this.onVertChange } value={ this.state.vert } /></div>
+          				<div>POSITION <input onChange={ this.onPositionChange } value={ this.state.position } /></div>
+          				<div>AAU PROGRAM <input onChange={ this.onAauProgramChange } value={ this.state.aauProgram } /></div>
+          				<div>AAU JERSEY <input onChange={ this.onAauJerseyChange } value={ this.state.aauJersey } /></div>
+          				<div>HIGH SCHOOL <input onChange={ this.onSchoolChange} value={ this.state.school } /></div>
+          				<div>HUDL PROFILE <input onChange={ this.onHudlChange } value={ this.state.hudl } /></div>
        				 </div>
     			</div>
     			<div className="col-md-4 input-box">
     				<div className="input-container">
             			<h4>ACADEMIC INFORMATION</h4>
-            			<div>GPA <input onChange={ this.onChange20 } value={ this.state.text20 } /></div>
-          				<div>ACT <input onChange={ this.onChange21 } value={ this.state.text21 } /></div>
-          				<div>CLASS RANK <input onChange={ this.onChange22 } value={ this.state.text22 } /></div>
+            			<div>GPA <input onChange={ this.onGpaChange } value={ this.state.gpa } /></div>
+          				<div>ACT <input onChange={ this.onActChange } value={ this.state.act } /></div>
+          				<div>CLASS RANK <input onChange={ this.onClassRankChange } value={ this.state.classRank } /></div>
        				</div>
     			</div>
     		</div>
@@ -151,4 +255,4 @@ var TodoApp3 = React.createClass({
   }
 });
 
-React.render(<TodoApp3 />, document.getElementById('generalInformation'));
+ReactDOM.render(<TodoApp3 />, document.getElementById('generalInformation'));
