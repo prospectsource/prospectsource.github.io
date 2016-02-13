f61783a39f72ab1e98d6888a7b0f38854ed04024
rustyhutchison <rustywill7778@gmail.com>
2016-02-13 01:04:36 -0600

    Updating Firebase with two inputs
    now moving on to mapping them with unique refs

--- js/rusty-ps-data-collection-react.js
+++ js/rusty-ps-data-collection-react.js
@@ -1,103 +1,147 @@
-/** @jsx React.DOM */
 
-var ContactInfoRow = React.createClass({
-    render: function() {
-        var contactProperties = ['FIRST NAME', 
-        				  'LAST NAME',
-        				  'PHONE (ATHLETE)',
-        				  'EMAIL (ATHLETE OR PARENT)',
-        				  'ADDRESS','CITY/STATE/ZIP',
-        				  'AAU COACH NAME',
-        				  'AAU COACH EMAIL',
-        				  'AAU COACH PHONE', 
-        				  'HS COACH NAME',
-          				  'HS COACH EMAIL',
-          				  'HS COACH PHONE'];
-        var lastCategory = null;
-    	this.props.products.forEach(function(product) {
-      if (product.category !== lastCategory) {
-        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-      }
-      rows.push(<ProductRow product={product} key={product.name} />);
-      lastCategory = product.category;
-    });
-        return (
-        		<div className="col-md-4 input-box">
-    				<div className="input-container">
-            			<h4>CONTACT INFORMATION</h4>
-            			<div>{contactProperties}<input onChange={ this.onChange } value={ this.state.text } /></div>
-            		</div>
-            	</div>;
-    }
+var ContactInfoLabels = React.createClass ({
+	  
+	  render(){
+            return (
+                <div>{this.props.category}</div>
+            );
+  	  }
 });
 
-var AthleticInfoRow = React.createClass({
-    render: function() {
-        var athleticProperties = ['ATHLETIC INFORMATION',
-            			  'GRADE/CLASS',
-            			  'HEIGHT',
-            			  'WEIGHT',
-            			  'VERTICAL JUMP', 
-            			  'POSITION',
-            			  'AAU PROGRAM',
-            			  'AAU JERSEY',
-            			  'HIGH SCHOOL',
-            			  'HUDL PROFILE'];
-        return (
-        		<div className="col-md-4 input-box">
-    				<div className="input-container">
-            			<h4>ATHLETIC INFORMATION</h4>
-            			<div>{athleticProperties}<input onChange={ this.onChange } value={ this.state.text } /></div>
-            		</div>
-            	</div>;
-    }
-});
-
-var AcademicInfoRow = React.createClass({
-    render: function() {
-        var athleticProperties = ['GPA',
-        						  'ACT',
-        						  'CLASS RANK'];
-        return (
-        		<div className="col-md-4 input-box">
-    				<div className="input-container">
-            			<h4>ACADEMIC INFORMATION</h4>
-            			<div>{properties}<input onChange={ this.onChange } value={ this.state.text } /></div>
-            		</div>
-            	</div>;
-    }
+var ContactInfoInputs = React.createClass ({
+	  	
+  		handleChange: function() {
+    			this.props.handleChange(
+    				this.refs.first.value,
+    				this.refs.last.value);
+  		},
+	  	
+	  	render(){
+        	return (
+                <div><input type="text"
+          					value={this.props.first}
+          					ref= "first"
+          					onChange={this.handleChange} />
+          			 <input type="text"
+          					value={this.props.last}
+          					ref= "last"
+          					onChange={this.handleChange} />
+            	</div>
+            );
+  		}
 });
 
 
+var ContactInfoLabelsAndInputs = React.createClass ({
+	  
+	 	
+	  render(){
+        var labeled = this.props.labels.map(function(label) {
+            return (
+            		<div key={label.id}>
+            			<ContactInfoLabels 
+            					category={label.category} key={label.category}/>
+            			
+                		
+            		</div>
+            );
+        }, this);
+    	return (
+    		<div>
+    		<div>{labeled}</div>
+    		<ContactInfoInputs 
+            					handleChange={this.props.handleChange} first={this.props.first} last={this.props.last} />
+        	</div>
+    	);
+  	  }
+});
 
 
+var ContactInfoContainer = React.createClass ({
+		
+		
+		
+		getInitialState: function() {
+    		return {
+      			first: '',
+      			last: ''
+    		};
+  		},
+
+		componentWillMount: function() {
+    		this.firebaseRef = new Firebase('https://sweltering-fire-7944.firebaseio.com');
+    		this.firebaseRef.limitToLast(25).on('value', function(dataSnapshot) {
+      		var prospects = [];
+      		dataSnapshot.forEach(function(childSnapshot) {
+        	var prospect = childSnapshot.val();
+        	prospect['.key'] = childSnapshot.key();
+        	prospects.push(prospect);
+      		}.bind(this));
+
+      		this.setState({
+        	prospects: prospects
+      		});
+    		}.bind(this));
+  		},
+
+  		componentWillUnmount: function() {
+    		this.firebaseRef.off();
+  		},
+
+  		handleUserInput: function(first,last) {
+    		this.setState({
+      			first: first,
+      			last: last
+    		});
+  		},
+		
+		handleSubmit: function(e) {
+   			e.preventDefault();
+    		if (this.state.first && this.state.first.trim().length !== 0) {
+      			this.firebaseRef.push({
+        			first: this.state.first,
+        			last: this.state.last
+      			});
+      			this.setState({
+        			first: '',
+        			last: ''
+      			});
+    		}
+  		},
+					
+	  render(){
+            return (
+        <form className="text-center" onSubmit={ this.handleSubmit }>
+            <div className="row">
+              <div className="col-md-4 input-box">
+    			<div className="input-container">
+            		<h4>CONTACT INFORMATION</h4>
+            		<div className="first-name-container">
+            			<ContactInfoLabelsAndInputs
+            					labels={this.props.labels} 
+    							first={this.state.first}
+    							last={this.state.last}
+          						handleChange={this.handleUserInput}/>		  
+            		</div>
+            	</div>
+              </div>
+            </div>
+        <button className="btn btn-default btn-large">NEXT</button>
+        </form>
+            );
+
+  		}
+});
 
 
-var PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
-var PLAYERS = [
-	{firstName:'Rusty', lastName:'Hutchison', phone:'5555555555', email:'rusty@gmail.com', address:'2314 Main', 
-	cityStZip:'Ankeny', aauCoach:'Ralph', aauCoachEmail:'ralph@ralph.com', aauCoachPhone:'6666666666', 
-	hsCoach:'Ronnie', hsCoachEmail:'Ronnie@ronnie.com',hsCoachPhone:'7777777777'},
-	{firstName:'Rusty2', lastName:'Hutchison2', phone:'5555555552', email:'rusty@gmail.com2', address:'2314 Main2', 
-	cityStZip:'Ankeny2', aauCoach:'Ralph2', aauCoachEmail:'ralph@ralph.com2', aauCoachPhone:'6666666662', 
-	hsCoach:'Ronnie2', hsCoachEmail:'Ronnie@ronnie.com2',hsCoachPhone:'7777777772'},
-];
-
-var playerCategories [
-	{firstName:'FIRST NAME', lastName:'LAST NAME', phone:'PHONE (ATHLETE)', email:'EMAIL (ATHLETE OR PARENT)', address:'ADDRESS', 
-	cityStZip:'CITY/STATE/ZIP', aauCoach:'AAU COACH NAME', aauCoachEmail:'AAU COACH EMAIL', aauCoachPhone:'AAU COACH PHONE', 
-	hsCoach:'HS COACH NAME', hsCoachEmail:'HS COACH EMAIL',hsCoachPhone:'HS COACH PHONE'
-];
+ var CONTACTLABELS = [{id: 1, category:'FIRST NAME', inputRefs: 'first' }, 
+						  {id: 2, category:'LAST NAME', inputRefs: 'last' },
+						  {id: 3, category:'PHONE (ATHLETE)', inputRefs: 'phone' },
+						  {id: 4, category:'EMAIL (ATHLETE OR PARENT)', inputRefs: 'email' },
+						  {id: 5, category:'ADDRESS', inputRefs: 'address' }
+							];
  
-React.render(
-    <ContactInfoRow products={PRODUCTS} />,
-    document.getElementById('container2')
+
+ReactDOM.render(<ContactInfoContainer labels={CONTACTLABELS}/>,
+    document.getElementById('players')
 );
