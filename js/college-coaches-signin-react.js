384966dc03948873d19b38b74565d2d3369a8875
rustyhutchison <rustywill7778@gmail.com>
2016-02-17 23:26:22 -0600

    go

--- js/college-coaches-signin-react.js
+++ js/college-coaches-signin-react.js
@@ -47,56 +47,35 @@ var CollegeCoachesSignIn = React.createClass({
   render: function() {
     return (
       
-      <section className="">
+    <section className="">
        	<form onSubmit={ this.handleSubmit }>
     		<div className="container">
     			<div className="row">
-		          <div><img src="img/ps-logo-sm.png" className="image-responsive center-button" alt=""/>
-		          </div>
-    				<div className="col-md-4 input-box">
-    					<div id="fee-info" className="">
-    						<h4><u>PROFILE FEE: $5/MONTH</u></h4>
-							<p><b>**FIRST 6MONTHS FREE**</b></p>
-							<div>Once registered, you will have
-								access to our premium services:
-								<ul>
-									<li>- Video Service</li>
-									<li>- Marketing Service</li>
-									<li>- Consulting Service</li>
-								</ul>
-								When premium services are purchased,
-								we will bill the credit card
-								on file. You can delete or remove
-								your credit card and profile at any
-								time.
-							</div>
+		          	<div id="logo" className="col-sm-offset-3 col-sm-6 input-box">
+		          		<div>
+		          			<img src="img/ps-logo-sm.png" className="image-responsive" alt=""/>
+		          		</div>
+		          	</div>
+    				<div id="signin" className="col-sm-offset-3 col-sm-6 input-box">
+    					<div className="input-container sign-in text-center">
+    						<div><input onChange={this.handleUserInput} value={ this.state.ccNumber } placeholder="Email"/></div>
+          					<div><input onChange={this.handleUserInput} value={ this.state.ccCode } placeholder="Password" /></div>
+    						<button id="question" className="btn btn-default">?</button>
+    						<button className="btn btn-default">Sign In</button>
     					</div>
     				</div>
-    				<div className="col-md-4 input-box">
-    					<div className="input-container cc-info">
-    						<h3>CC Information</h3>
-    						<div>CC Number<input onChange={this.handleUserInput} value={ this.state.ccNumber } /></div>
-          					<div>Security Code<input onChange={this.handleUserInput} value={ this.state.ccCode } /></div>
-          					<div>Expiration <input onChange={this.handleUserInput} value={ this.state.ccExpiration } /></div>
-          					<div>Name On Card<input onChange={this.handleUserInput} value={ this.state.ccName } /></div>
-    					</div>
-    				</div>
-    				<div className="col-md-4 input-box">
-    					<div className="input-container cc-info">
-    						<h3>Billing Address</h3>
-    						<div>Street<input onChange={this.handleUserInput} value={ this.state.billingStreet } /></div>
-          					<div>City<input onChange={this.handleUserInput} value={ this.state.billingCity } /></div>
-          					<div>State <input onChange={this.handleUserInput} value={ this.state.billingState } /></div>
-          					<div>Zip<input onChange={this.handleUserInput} value={ this.state.billingZip } /></div>
+    				<div id="signin" className="col-sm-offset-3 col-sm-6 input-box">
+    					<div id="links" className="input-container text-center">
+    						<h5>College basketball prospect? <a href="prospect-registration" >Join now</a>.</h5>
+							<h5>College basketball coach? <a href="coach-registration" >Join now</a>.</h5>
     					</div>
     				</div>
     			</div>
     	  	</div>
-          	<button className="btn btn-default btn-large center-button">Finish</button>
     	</form>
     </section>
     );
   }
 });
 
-React.render(<CollegeCoachesSignIn />, document.getElementById('coaches-signin'));
+ReactDOM.render(<CollegeCoachesSignIn />, document.getElementById('coaches-signin'));
